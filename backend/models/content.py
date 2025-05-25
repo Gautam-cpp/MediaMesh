70e0896d57e855ef0c8aee9a91cd64f1a4fb97e6
gautam-cpp <gautamprajapat2509@gmail.com>
2025-05-25 13:24:37 +0530

    change the models

--- backend/models/content.py
+++ backend/models/content.py
@@ -5,17 +5,10 @@ from datetime import datetime
 from sqlalchemy import Column, JSON
 from uuid import UUID, uuid4
 
-class ContentTypeEnum(str, Enum):
-    image = "image"
-    video = "video"
-    audio = "audio"
-    article = "article"
-    tweet = "tweet"
-
 class Content(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     link: str = Field(index=True)  
-    content_type: ContentTypeEnum = Field(index=True)
+    content_type: str = Field(index=True)
     title: str = Field(index=True)
     tags: List[str] = Field(sa_column=Column(JSON))
     user_id: UUID = Field(foreign_key="user.id")
